61bf7d46a6bb407ad24c2d0d1678c771b1870971
Anatoliy Chudnov <tolik.chudnov@gmail.com>
2023-09-25 17:36:03 +0300

    fix: add celebrate for user route

--- routes/users.js
+++ routes/users.js
@@ -1,8 +1,22 @@
 const router = require('express').Router();
+const { Joi, celebrate } = require('celebrate');
 const auth = require('../middlewares/auth');
 const { getUser, updateProfile } = require('../controllers/users');
 
-router.get('/me', auth, getUser);
-router.patch('/me', auth, updateProfile);
+router.get('/me', celebrate({
+  cookies: Joi.object().keys({
+    jwt: Joi.string().required(),
+  }),
+}), auth, getUser);
+
+router.patch('/me', celebrate({
+  cookies: Joi.object().keys({
+    jwt: Joi.string().required(),
+  }),
+  body: Joi.object().keys({
+    name: Joi.string().min(2).max(30),
+    about: Joi.string().min(2).max(30),
+  }),
+}), auth, updateProfile);
 
 module.exports = router;
